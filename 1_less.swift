3dfffc5a9d9157e534d8cdb070cd0bab8c9f23ae
Akari <zartanec13@gmail.com>
2021-09-13 16:50:02 +0300

    1 lesson

---
+++ 1_less.swift
@@ -0,0 +1,37 @@
+//
+//  1_less.swift
+//  Test
+//
+//  Created by Ярослав on 13.09.2021.
+//
+
+import Foundation
+// 1 ticket
+let Parametr:Double = 0
+let equation = Parametr * Parametr
+
+if equation == 0 {
+    print("GJ")
+} else if equation > 0 {
+    print("O kurwa")
+}
+
+// 2 ticket
+let Katet:Double = 3
+let AnotherKatet:Double = 4
+let Square = (Katet * AnotherKatet) / 2
+let Hypotenuse = pow((Katet * Katet + AnotherKatet * AnotherKatet), 0.5)
+let Perimeter = Hypotenuse + Katet + AnotherKatet
+print("Square pyramid = \(Square)")
+print("Gipotenuza pyramid = \(Hypotenuse)")
+print("Perimeter pyramid = \(Perimeter)")
+
+// 3 ticket
+let summaVklada: Double = 666
+let RusProcent: Double = 3/100/12
+let Year: Array<Int> = [1,2,3,4,5]
+var invest = Vklad
+for _ in Year {
+    invest = invest + invest * RusProcent
+}
+print("Profit \(invest - Vklad)")
