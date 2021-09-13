9c509c1a35e8e1bdaf8a40ca6771aaf738f558ca
Akari <zartanec13@gmail.com>
2021-09-13 19:43:07 +0300

    Initial Commit

---
+++ Shared/GBApp.swift
@@ -0,0 +1,17 @@
+//
+//  GBApp.swift
+//  Shared
+//
+//  Created by Ярослав on 13.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct GBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
